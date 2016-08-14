4e8c877a3547da524a743398201e3e3877183fc8
Liang Xiang <xlvector@gmail.com>
2016-08-14 21:15:50 +0800

    config

--- init.el
+++ init.el
@@ -1,18 +1,58 @@
 (setq make-backup-files nil)
 (setq column-number-mode t)
+(setq inhibit-startup-screen t)
+(setq truncate-partial-width-windows t)
+(menu-bar-mode nil)
+(setq column-number-mode t)
+(display-time-mode t)
+(setq display-time-24hr-format t)
+
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("marmalade" . "https://marmalade-repo.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
+
+(add-to-list 'package-archives
+	     '("popkit" . "http://elpa.popkit.org/packages/"))
+
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
+
+;;(setq compilation-auto-jump-to-first-error nil)
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
-;;line number
-;;(setq linum-format "%d ")
-;;(global-linum-mode t)
+(add-to-list 'load-path "~/.emacs.d/lisp/emacs-neotree/")
+
+(defun command-line-diff (switch)
+  (let ((file1 (pop command-line-args-left))
+        (file2 (pop command-line-args-left)))
+    (ediff file1 file2)))
+
+(add-to-list 'command-switch-alist '("-diff" . command-line-diff))
+(setq ediff-split-window-function 'split-window-horizontally)
+(global-set-key (kbd "C-.") 'scroll-all-mode)
 
 (require 'linum)
+(setq linum-format "%4d ")
+(global-linum-mode t)
 
-(standard-display-ascii ?\t "^")
+;;(standard-display-ascii ?\t "^")
 
+(require 'window-numbering)
+(window-numbering-mode 1)
 
-(require 'color-theme)
-(color-theme-blue-sea)
+(defun create-tags (dir-name)
+  "Create tags file."
+  (interactive "DDirectory: ")
+  (eshell-command
+   (format "find %s -type f -name \"*.[ch]*\" | etags -" dir-name))
+)
+
+(require 'molokai-theme)
+;;(require 'monokai-theme)
+;;(require 'color-theme)
+;;(color-theme-deep-blue)
 ;;(load-theme 'wombat t)
 
 ;;(require 'git-emacs)
@@ -41,7 +81,7 @@
   (add-hook 'before-save-hook 'gofmt-before-save)
   (if (not (string-match "go" compile-command))
       (set (make-local-variable 'compile-command)
-	   "go build -v && go test -v && go vet"))
+      "go build -v && go test -v && go vet"))
   (local-set-key (kbd "M-.") 'godef-jump))
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 
@@ -54,11 +94,18 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
+(unless (fboundp 'window-body-width)
+  ;; Compatibility for Emacs pre-24
+  (defalias 'window-body-width 'window-width))
+
+(unless (boundp 'directory-files-no-dot-files-regexp)
+  (defconst directory-files-no-dot-files-regexp "^\\([^.]\\|\\.\\([^.]\\|\\..\\)\\).*"
+    "Regexp that matches anything except `.' and `..'."))
+
 (require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
-
-
-(global-set-key (kbd "<f5>") 'compile)
+(global-set-key (kbd "<f7>") 'neotree-toggle)
+(global-set-key (kbd "<f12>") 'compile)
+(setq compile-command "make opt -j10")
 (global-set-key (kbd "<f4>") 'shell)
 
 (require 'web-mode)
---
+++ lisp/monokai-theme.el
@@ -0,0 +1,116 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(show-paren-mode t)
+ '(text-mode-hook (quote (turn-on-auto-fill text-mode-hook-identify))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
+(add-to-list 'load-path "/home/diguojin/plugin/")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "/home/diguojin/plugin//ac-dict")
+(ac-config-default)
+;; c code indentation
+(setq c-default-style
+      '((c-mode . "k&r") (other . "gnu")))
+(setq c-basic-offset 4)
+
+;;==================================================================================================
+;; 光标颜色设置
+(set-cursor-color "green")
+
+;;; monokai-theme.el --- REQUIRES EMACS 24: Monokai Color Theme for Emacs.
+
+;; Copyright (C) 2012 Lorenzo Villani.
+;;
+;; Author: Lorenzo Villani <lorenzo@villani.me>
+;; URL: https://github.com/lvillani/el-monokai-theme
+;; Version: 0.0.10
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, version 3 of the License.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.
+
+(unless (>= 24 emacs-major-version)
+  (error "monokai-theme requires Emacs 24 or later."))
+
+(deftheme monokai
+  "Monokai color theme")
+
+(let ((monokai-blue-light "#89BDFF")
+      (monokai-gray "#595959")
+      (monokai-gray-darker "#383830")
+      (monokai-gray-darkest "#141411")
+      (monokai-gray-lightest "#595959")
+      (monokai-gray-light "#E6E6E6")
+      (monokai-green "#A6E22A")
+      (monokai-green-light "#A6E22E")
+      (monokai-grey-dark "#272822")
+      (monokai-magenta "#F92672")
+      (monokai-purple "#AE81FF")
+      (monokai-purple-light "#FD5FF1")
+      (monokai-yellow "#E6DB74")
+      (monokai-yellow-dark "#75715E")
+      (monokai-yellow-light "#F8F8F2"))
+  (custom-theme-set-faces
+   'monokai
+   ;; Frame
+   `(default ((t (:foreground ,monokai-yellow-light :background ,monokai-grey-dark))))
+   `(cursor ((t (:foreground ,monokai-magenta))))
+   `(hl-line ((t (:background ,monokai-gray-darkest))))
+   `(minibuffer-prompt ((t (:foreground ,monokai-yellow-dark))))
+   `(modeline ((t (:background ,monokai-gray-lightest :foreground ,monokai-gray-light))))
+   `(region ((t (:background ,monokai-gray-darker))))
+   `(show-paren-match-face ((t (:background ,monokai-gray-lightest))))
+   ;; Main
+   `(font-lock-builtin-face ((t (:foreground ,monokai-green))))
+   `(font-lock-comment-face ((t (:foreground ,monokai-yellow-dark))))
+   `(font-lock-constant-face ((t (:foreground ,monokai-purple))))
+   `(font-lock-doc-string-face ((t (:foreground ,monokai-yellow))))
+   `(font-lock-function-name-face ((t (:foreground ,monokai-green))))
+   `(font-lock-keyword-face ((t (:foreground ,monokai-magenta))))
+   `(font-lock-string-face ((t (:foreground ,monokai-yellow))))
+   `(font-lock-type-face ((t (:foreground ,monokai-blue-light))))
+   `(font-lock-variable-name-face ((t (:foreground ,monokai-magenta))))
+   `(font-lock-warning-face ((t (:bold t :foreground ,monokai-purple-light))))
+   ;; CUA
+   `(cua-rectangle ((t (:background ,monokai-gray-darkest))))
+   ;; IDO
+   `(ido-first-match ((t (:foreground ,monokai-purple))))
+   `(ido-only-match ((t (:foreground ,monokai-green))))
+   `(ido-subdir ((t (:foreground ,monokai-blue-light))))
+   ;; ECB
+   `(ecb-default-highlight-face ((t (:foreground ,monokai-green))))
+   ;; Whitespace
+   `(whitespace-space ((t (:foreground ,monokai-gray))))
+   ;; Yasnippet
+   `(yas/field-highlight-face ((t (:background ,monokai-gray-darker))))))
+
+;;;###autoload
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+	       (file-name-as-directory (file-name-directory load-file-name)))
+  (when (not window-system)
+    (custom-set-faces '(default ((t (:background "nil")))))))
+
+(provide-theme 'monokai)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
+
+;;; monokai-theme.el ends here
